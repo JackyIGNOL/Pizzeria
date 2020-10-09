5336d74188386dbdb04bfd69e4acb969fa9cda91
Jacky IGNOL <jacky.ignol@gmail.com>
2020-10-09 08:55:06 +0200

    petit changement hier

---
+++ script2.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  
+    var tbbutton = document.querySelectorAll(".menuHCell");
+
+    var tbframe = document.querySelectorAll(".menuMain");
+
+    tbbutton.forEach(element => {
+        console.log(element);
+    });
+    tbframe.forEach(element => {
+        console.log(element);
+    });
+
+    /* buttonA.addEventListener("click", function () {
+    }); */
+})
